98320083ed84a56c790d2113bf9bf0af06e16079
Sarit Ritwirune <sarit@elcolie.com>
2018-08-06 21:07:23 +0700

    ex4

---
+++ exercises/ex4.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from numpy import ndarray
+
+
+def main():
+    number = 7
+    df = pd.read_csv('../large_files/train.csv')
+    M = df.values
+    m1 = M[M[:, 0] == number]  # Select label 1
+    no_label_m1 = m1[:, 1:]
+    row, col = no_label_m1.shape
+    sum_1 = np.sum(no_label_m1, axis=0)
+    image_sum_1 = sum_1.reshape(28, 28)
+    av_sum1 = image_sum_1 / row
+
+    str_filename = f'm{number}.png'
+    plt.imshow(av_sum1, cmap='gray')
+    plt.savefig(str_filename)
+    from PIL import Image
+    img = Image.open(str_filename)
+    img.show()
+
+
+if '__name__' == '__main__':
+    main()
