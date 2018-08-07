2433fe79e7bb18b03b9502816905c664be35632c
Sarit Ritwirune <sarit@elcolie.com>
2018-08-07 21:32:24 +0700

    ex6 with efficient code

--- exercises/ex6.py
+++ exercises/ex6.py
@@ -1,20 +1,25 @@
-import operator
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from numpy import ndarray
+
+
+def rbool(x: float):
+    return x > 0
+
+
+def xor(x: bool, y: bool) -> bool:
+    return x != y
 
 
 def main():
-    n = 1000
-    X = 2 * np.random.random(n) - 1
-    Y = 2 * np.random.random(n) - 1
-    for x, y in zip(X, Y):
-        if x * y < 0:
-            plt.scatter(x, y, color='r')
-        else:
-            plt.scatter(x, y, color='b')
+    pp = np.random.uniform(-1, 1, (1000, 2))
+    tmp = rbool(pp)
+    tmp2 = xor(tmp[:, 0], tmp[:, 1])
+
+    col = lambda x: 'r' if x else 'b'
+    vfunc = np.vectorize(col)
+    ans = vfunc(tmp2)
+
+    plt.scatter(pp[:, 0], pp[:, 1], c=ans)
     str_filename = '2d-uniform.png'
     plt.savefig(str_filename)
     from PIL import Image
