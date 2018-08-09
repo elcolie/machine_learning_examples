06ecbd5af367ff9aeb58ca9f96da0bbf951402e6
Sarit Ritwirune <sarit@elcolie.com>
2018-08-10 01:55:35 +0700

    ex8 Use `c` not `cmap`

--- exercises/ex8.py
+++ exercises/ex8.py
@@ -21,11 +21,11 @@ def main():
     for i in range(1, 4):
         r = np.abs(np.random.normal(0, 0.2, n)) + 1
         x, y = get_spiral(r, theta(omega, time), -np.pi / 3.0 + i * np.pi * 2.0 / 3.0)
-        plt.scatter(x, y, cmap='blue')
+        plt.scatter(x, y, c='blue')
 
         r2 = np.abs(np.random.normal(0, 0.2, n)) + 1
         x2, y2 = get_spiral(r2, theta(omega, time), i * np.pi * 2.0 / 3.0)
-        plt.scatter(x2, y2, cmap='red')
+        plt.scatter(x2, y2, c='red')
 
     str_filename = 'ex8.png'
     plt.savefig(str_filename)
